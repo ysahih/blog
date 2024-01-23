97ecfcba55eb32abbfb12c3dfd3d3a2bc97df78f
ysahih <ysahih@student.1337.ma>
2024-01-23 15:06:32 +0100

    add: alerts

--- word-day-game/srcs/wordy.js
+++ word-day-game/srcs/wordy.js
@@ -12,6 +12,7 @@ async function init(){
 
     let Guess = "";
     let Row = 0;
+    let finished = false;
 
     const res = await fetch("https://words.dev-apis.com/word-of-the-day");
     const resObj = await res.json();
@@ -29,27 +30,50 @@ async function init(){
     }
 
 
-    function enter(){
+    async function enter(){
+
         if(Guess.length < LENGTH)
             return ;
-        for (let i = 0; i<LENGTH; i++){
-            
-            if (Guess.charAt(i) === word.charAt(i) ){
-                
-                letters[Row * LENGTH + i].classList.add("correct");
-                document.querySelector("." + Guess.charAt(i)).classList.add("correct");
-            }   
+
+        const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${Guess}`);
+        const resObj = await response.json();
+        
+        if (resObj.title === "No Definitions Found") {
+            alert("Word not in dictionary");
+            return;
         }
+        
         for (let i = 0; i<LENGTH; i++){
             if (Guess.charAt(i) !== word.charAt(i) && word.includes(Guess.charAt(i))){
                 letters[Row * LENGTH + i].classList.add("close");
                 document.querySelector("." + Guess.charAt(i)).classList.add("close");
             }
-            
+            else  if (Guess.charAt(i) !== word.charAt(i)){
+                letters[Row * LENGTH + i].classList.add("wrong");
+                document.querySelector("." + Guess.charAt(i)).classList.add("wrong");
+            }
         }
+        
+        for (let i = 0; i<LENGTH; i++){
+            
+            if (Guess.charAt(i) === word.charAt(i) ){
+                letters[Row * LENGTH + i].classList.add("correct");
+                document.querySelector("." + Guess.charAt(i)).classList.add("correct");
+            }
+        }
+        
         Row++;
+        
+        if (Row == 6 && finished)
+            alert("You lose!");
+    
+        if (word == Guess){
+            alert("You Win!");
+            finished = true;
+        }
+
         Guess = '';
-    }
+}
 
     function backSpace(){
         Guess = Guess.substring(0, Guess.length - 1);
@@ -58,6 +82,8 @@ async function init(){
 
     function switchCase(input){
 
+        if (finished || Row == 6)
+            return;
         if (input == "Enter" || input == "ENTER")
             enter();
         else if (input == "Backspace" || input == "backspace")
@@ -73,7 +99,6 @@ async function init(){
     document.addEventListener("keydown", function (event){
         switchCase(event.key);
     })
-    
 }
 
 init();
